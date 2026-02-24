14b232625579baabed22f05a942357984a51d107
ecky16 <eckyrahmad769@gmail.com>
2026-02-25 03:43:18 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
       const url = window.URL.createObjectURL(blob);
       const a = document.createElement('a');
       a.href = url;
-      a.download = 'HASIL_REKON_FINAL.xlsx';
+      a.download = 'HASIL_BOQ_FINAL.xlsx';
       document.body.appendChild(a);
       a.click();
       a.remove();
